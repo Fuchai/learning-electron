8dd3fc579e1837fa39d1ca90ba942bbdd71979ca
Fuchai <huyaojie168@me.com>
2018-02-07 22:29:59 -0600

    shortcut complete

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+//var app = require('electron').app;
+//var BrowserWindow = require('electron').BrowserWindow
 
 var mainWindow = null;
+//var globalShortcut = require('electron').GlobalShortcut;
+
+const{app,globalShortcut,BrowserWindow}=require('electron');
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
@@ -13,10 +16,18 @@ app.on('ready', function() {
         width: 368
     });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    globalShortcut.register('ctrl+shift+1',function(){
+      mainWindow.webContents.send('global-shortcut',1);
+      console.log("ctrl+shift+1 sent");
+    });
+    globalShortcut.register('ctrl+shift+2',function(){
+      mainWindow.webContents.send('global-shortcut',2);
+    });
 });
 
-var ipc = require('ipc');
+var ipc = require('electron').ipcMain;
 
 ipc.on('close-main-window', function () {
     app.quit();
